23569a04b2b346f28302bca5b8a935fa5580b2b4
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-02-24 22:49:56 -0300

    Add files via upload

---
+++ meu_portfolio/main.js
@@ -0,0 +1,21 @@
+const meuGitHub = document.querySelector("#github");
+const contact = document.querySelector("#contact");
+const hireMe = document.querySelector("#hire");
+const instagram = document.querySelector("#insta");
+const discord = document.querySelector("#discord");
+
+meuGitHub.onclick = function() {
+    window.location.href = "https://github.com/rlimazzz";
+}
+
+hireMe.onclick = function() {
+    window.location.href = "https://www.linkedin.com/in/ryan-gabryel-99ba99283/";
+}
+
+instagram.onclick = function() {
+    window.location.href = "https://www.instagram.com/ryan_gabr11/";
+}
+
+discord.onclick = function() {
+    window.location.href = "https://discord.gg/SFsktZUK";
+}
